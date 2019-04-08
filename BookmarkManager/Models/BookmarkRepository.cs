010966777f48b93c4ad55e3faca39b1ddf31fd50
Philip S <sulinski@shernet.sheridancollege.ca>
2019-04-08 16:06:03 -0400

    bookmark repo wip

--- BookmarkManager/Models/BookmarkRepository.cs
+++ BookmarkManager/Models/BookmarkRepository.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Web;
+using System.Web.Http;
+using Lab6.Filters;
 
 namespace BookmarkManager.Models
 {
@@ -34,38 +37,78 @@ namespace BookmarkManager.Models
             _db = new DatabaseModel();
         }
 
+        [AllowAnonymous]
+        [HttpGet]
         public Bookmark GetBookmark(int id)
         {
-            return new Bookmark();
+            if (id == null)
+                throw new HttpRequestException("Id is required");
+
+            var bookmark = _db.Bookmarks.Find(id);
+
+            if(bookmark == null)
+                throw new HttpResponseException(HttpStatusCode.Conflict);
+
+            return bookmark;
         }
 
+        [JwtAuthentication]
+        [HttpPost]
         public Bookmark CreateBookmark(User user)
         {
+            //link, title, date, author, username?
+
             return new Bookmark();
         }
 
+        [JwtAuthentication]
+        [HttpPut]
         public HttpStatusCode EditBookmark(int id)
         {
+            //pass info to edit? or bookmark as well
+
+            //link, title, date, author, username?
+
             return HttpStatusCode.Accepted;
         }
 
+        //redundant?
         public HttpStatusCode SaveBookmark()
         {
             return HttpStatusCode.Accepted;
         }
 
+        [JwtAuthentication]
+        [HttpDelete]
         public HttpStatusCode DeleteBookmark(int id)
         {
+            var bookmark = _db.Bookmarks.Find(id);
+
+            if(bookmark == null)
+                throw new HttpResponseException(HttpStatusCode.Conflict);
+
+            _db.Bookmarks.Remove(bookmark);
+
             return HttpStatusCode.Accepted;
         }
 
+        [AllowAnonymous]
+        [HttpGet]
         public Bookmark GetBookmark(string title)
         {
+            var bookmark = _db.Bookmarks.SingleOrDefault(x => x.Title.ToLower() == title.ToLower());
+
+            if(bookmark == null)
+                throw new HttpResponseException(HttpStatusCode.Conflict);
+
             return new Bookmark();
         }
 
+        [AllowAnonymous]
+        [HttpGet]
         public IEnumerable<User> GetBookmarks(User user)
         {
+            //change to List<Bookmark> and do a where
             return new List<User>();
         }
 
