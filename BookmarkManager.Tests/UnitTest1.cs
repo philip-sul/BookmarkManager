7ddf7086412cfb5c66998e14c5001fa75903b013
Chris Masters <chrismasters5483@gmail.com>
2019-04-13 23:36:42 -0400

    users

---
+++ BookmarkManager.Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BookmarkManager.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CreateUser_Success_ReturnUser()
+        {
+            var userRepo = new Mock<IUserRepository>();
+            var controller = new UserController();
+            var user = new Mock<User>();
+
+            userRepo.Setup(x => x.CreateUser(user.Object));
+            userRepo.Verify(y => y.Save());
+
+            controller.CreateUser(new CreateJson { });
+
+            userRepo.Verify(y => y.Save());
+        }
+
+    }
+}
